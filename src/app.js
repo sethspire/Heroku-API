9c283288e59c8087a73b2e297c577ee4fcffc2cb
Seth Spire <sspire2@eagles.bridgewater.edu>
2022-02-14 10:13:36 -0500

    update API

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+console.log("started")
 const express = require('express')
 require('./db/mongoose')
 
--- src/models/user.js
+++ src/models/user.js
@@ -10,18 +10,20 @@ const userSchema = new Schema({
       trim: true,
       lowercase: true
     },
+
     password: {
         type: String,
         required: true,
         trim: true,
         minLength: 8
-      },
-      name: { 
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-      }
+    },
+    
+    name: { 
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    }
   })
 
   const User = mongoose.model('User', userSchema);
