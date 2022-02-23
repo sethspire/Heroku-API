7eb5940a3d1b25c329b495b30ffe867cfea5d06f
Seth Spire <sspire2@eagles.bridgewater.edu>
2022-02-23 00:34:02 -0500

    add login and user functions

--- src/routers/users.js
+++ src/routers/users.js
@@ -34,4 +34,71 @@ router.post('/users/logout', auth, async (req, res) => {
   }
 })
 
+// login user
+router.post('/users/login', async (req, res) => {
+
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const token = await user.generateAuthToken()
+    res.status(200).send({user, token})
+  } 
+  catch (e) {    
+    res.status(400).send()
+  }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user)
+})
+
+router.patch('/users/me', auth, async(req, res) => {
+  const mods = req.body
+  const props = Object.keys(mods)
+  const modifiable = ['email', 'password']
+  const isValid = props.every((prop) => modifiable.includes(prop))
+
+  if (!isValid) {
+      return res.status(400).send({ error: 'Invalid updates.' })
+  }
+
+  try {
+      const user = req.user
+      props.forEach((prop) => user[prop] = mods[prop])
+      await user.save()
+      res.send(user)
+  } catch (e) {
+      res.status(400).send()
+  }
+})
+
+router.patch('/users/me', auth, async(req, res) => {
+  const mods = req.body
+  const props = Object.keys(mods)
+  const modifiable = ['email', 'password']
+  const isValid = props.every((prop) => modifiable.includes(prop))
+
+  if (!isValid) {
+      return res.status(400).send({ error: 'Invalid updates.' })
+  }
+
+  try {
+      const user = req.user
+      props.forEach((prop) => user[prop] = mods[prop])
+      await user.save()
+      res.send(user)
+  } catch (e) {
+      res.status(400).send()
+  }
+})
+
+router.delete('/users/me', auth, async (req, res) => {
+  try {
+    await User.deleteOne({_id: req.user._id})
+    res.send(req.user)
+  } 
+  catch (e) {
+    res.status(500).send()
+  }
+})
+
 module.exports = router
