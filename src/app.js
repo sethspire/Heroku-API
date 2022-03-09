43db8c18dfc088d636b7fed33f224f75499e0dc7
Seth Spire <sspire2@eagles.bridgewater.edu>
2022-03-08 23:58:50 -0500

    allow connections from everywhere

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express = require('express')
 require('./db/mongoose')
-
+const cors = require('cors');
 const userRouter = require('./routers/users')
 
 const app = express()
+
 app.use(express.json())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(userRouter)
+app.use(cors())
 
 const port = process.env.PORT
 
