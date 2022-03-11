36308e9f3e7811bb68ddb573c465627f90b674b4
Seth Spire <sspire2@eagles.bridgewater.edu>
2022-03-11 00:09:46 -0500

    CORS update

--- src/app.js
+++ src/app.js
@@ -6,14 +6,13 @@ const userRouter = require('./routers/users')
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 app.use(userRouter)
-app.use(cors())
 
 const port = process.env.PORT
 
